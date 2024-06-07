3a60f55cbf0c7336c20dc040c55f96b1fd3441ce
Davi Henrique dos Santos Silva <davi.henrique.silva@outlook.com>
2024-06-07 18:14:07 -0300

    chore: config

--- ApiTestePraticoDesenvolvedor/Program.cs
+++ ApiTestePraticoDesenvolvedor/Program.cs
@@ -1,3 +1,11 @@
+﻿using ApiTestePraticoDesenvolvedor.Application.Commands.Compra.Perfil;
+using ApiTestePraticoDesenvolvedor.Application.Validations;
+using ApiTestePraticoDesenvolvedor.Infra.DatabaseContext;
+using ApiTestePraticoDesenvolvedor.Infra.Perfil;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,49 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure Database
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<Context>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
+
+// Dependency injection Service
+builder.Services.Scan(scan => scan
+    .FromApplicationDependencies(a => a.FullName!.StartsWith("ApiTestePraticoDesenvolvedor.Application"))
+    .AddClasses(c => c.Where(n => n.FullName!.EndsWith("Service")))
+    .AsImplementedInterfaces()
+);
+
+// Dependency injection Repository
+builder.Services.Scan(scan => scan
+    .FromApplicationDependencies(a => a.FullName!.StartsWith("ApiTestePraticoDesenvolvedor.Infra"))
+    .AddClasses(c => c.Where(n => n.FullName!.EndsWith("Repository")))
+    .AsImplementedInterfaces()
+);
+
+// Fluentvalidation
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(typeof(ValidateModelStateAttribute)); // Add the filter globally
+    options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true;
+}).ConfigureApiBehaviorOptions(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;  // Disable automatic model validation
+});
+
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddFluentValidationClientsideAdapters();
+builder.Services.AddValidatorsFromAssemblyContaining<ContaValidator>();
+
+
+// AutoMapper
+builder.Services.AddAutoMapper(
+    typeof(CompraServiceProfile),
+    typeof(CompraRepositoryProfile)
+);
 
 var app = builder.Build();
 
