4d3f5de5b85dbdf79f27dd41b7f337c2eb3ee825
Davi Henrique dos Santos Silva <davi.henrique.silva@outlook.com>
2024-06-07 18:15:15 -0300

    feat: add domin

---
+++ ApiTestePraticoDesenvolvedor.Domain/Entities/ContaEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiTestePraticoDesenvolvedor.Domain.Entities;
+
+public class ContaEntity
+{
+    public Guid IdConta { get; set; }
+    public required string Nome { get; set; }
+    public double ValorOriginal { get; set; }
+    public double ValorCorrigido { get; set; }
+    public DateTime DataVencimento { get; set; }
+    public DateTime DataPagamento { get; set; }
+    public int DiasAtrasados { get; set; }
+}
