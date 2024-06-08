2eb58292e5972f4e12e4f9e2b3153ecb6e88ca4c
Davi Henrique dos Santos Silva <davi.henrique.silva@outlook.com>
2024-06-08 09:08:07 -0300

    test: add teste incluir

---
+++ ApiTestePraticoDesenvolvedor.Tests/Application/Commands/Compra/CompraServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using ApiTestePraticoDesenvolvedor.Application.Commands.Compra;
+using ApiTestePraticoDesenvolvedor.Application.Commands.Compra.Enum;
+using ApiTestePraticoDesenvolvedor.Application.Commands.Compra.Perfil;
+using ApiTestePraticoDesenvolvedor.Domain.Dto;
+using ApiTestePraticoDesenvolvedor.Infra.Interfaces;
+using ApiTestePraticoDesenvolvedor.Tests.SharedKernel.Mock.Application.Commands.Compra.Responses;
+using AutoMapper;
+using FluentAssertions;
+using Moq;
+
+namespace ApiTestePraticoDesenvolvedor.Tests.Application.Commands.Compra;
+public class CompraServiceTests
+{
+    private readonly Mock<ICompraRepository> _compraRepository;
+    private readonly IMapper _mapper;
+
+    public CompraServiceTests()
+    {
+        _compraRepository = new Mock<ICompraRepository>();
+
+        var config = new MapperConfiguration(opt =>
+        {
+            opt.AddProfile(new CompraServiceProfile());
+        });
+
+        _mapper = config.CreateMapper();
+    }
+
+    [Fact]
+    public void DeveIncluirPagamento()
+    {
+
+        _compraRepository.Setup(r => r.CadastrarConta(It.IsAny<ContaDto>())).Returns(true);
+        _compraRepository.Setup(r => r.VerificaPagamento(It.IsAny<DateTime>())).Returns(true);
+
+        var compraService = new CompraService(_compraRepository.Object, _mapper);
+
+        var result = compraService.Incluir(CompraIncluirRequestMock.GetMocked());
+
+        result.Should().NotBeNull();
+
+        result.Status.Should().Be(StatusConta.Pago);
+        result.Menssagens.First().Should().BeEquivalentTo("Conta Paga Com Sucesso.");
+    }
+}
