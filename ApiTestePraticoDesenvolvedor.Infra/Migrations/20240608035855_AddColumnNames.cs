c0cdccab94a405f0d140381211afef1a08903ac0
Davi Henrique dos Santos Silva <davi.henrique.silva@outlook.com>
2024-06-08 01:23:35 -0300

    refactor: add  Atribute ExcludeFromCodeCoverage

--- ApiTestePraticoDesenvolvedor.Infra/Migrations/20240608035855_AddColumnNames.cs
+++ ApiTestePraticoDesenvolvedor.Infra/Migrations/20240608035855_AddColumnNames.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace ApiTestePraticoDesenvolvedor.Infra.Migrations
 {
     /// <inheritdoc />
+    [ExcludeFromCodeCoverage]
     public partial class AddColumnNames : Migration
     {
         /// <inheritdoc />
