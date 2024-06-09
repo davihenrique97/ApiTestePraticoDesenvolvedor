db61725891d24067413f4ad5831eebc81dd60245
Davi Henrique dos Santos Silva <davi.henrique.silva@outlook.com>
2024-06-09 09:26:37 -0300

    feat: add migration RegraCalculo

---
+++ ApiTestePraticoDesenvolvedor.Infra/Migrations/20240608221658_RegraCalculo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiTestePraticoDesenvolvedor.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class RegraCalculo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "REGRA_CALCULO_CONTA",
+                table: "CONTA_DATABASE",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "REGRA_CALCULO_CONTA",
+                table: "CONTA_DATABASE");
+        }
+    }
+}
